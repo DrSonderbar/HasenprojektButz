82ea91c62761044244b12502f57a56a3aa3f0f68
DrSonderbar <l.methner@gmx.de>
2016-03-15 08:54:30 +0100

    revert

--- DASMOVENDEHASENPROJEKT/FinnischesHasenprojekt/src/finnischeshasenprojekt/FinnischesHasenprojekt.java
+++ DASMOVENDEHASENPROJEKT/FinnischesHasenprojekt/src/finnischeshasenprojekt/FinnischesHasenprojekt.java
@@ -1,11 +1,7 @@
 package finnischeshasenprojekt;
 
-<<<<<<< HEAD
-import java.awt.Frame;
-
-=======
 import frameapi.*;
->>>>>>> parent of dbe3299... Zufallszahlen
+
 import javax.swing.JButton;
 
 public class FinnischesHasenprojekt {
@@ -15,30 +11,28 @@ public class FinnischesHasenprojekt {
     public static String[][] Speicher = new String[14][14];
 
     public static JButton btn = new JButton("hallo");
-<<<<<<< HEAD
     private static int x;
     private static int y;
-=======
->>>>>>> parent of dbe3299... Zufallszahlen
+
 
     public static void main(String[] args) {
 
         setFelder();
 
-        Frame.initFrame();
-        Frame.initString(feld.length, feld);
-        Frame.setPlayground();
+        FrameAPI.initFrame();
+        FrameAPI.initString(feld.length, feld);
+        FrameAPI.setPlayground();
 
         do {
-            if (Frame.newRound) {
+            if (FrameAPI.newRound) {
 
                 moveHasen();
                 moveFüchse();
-                Frame.initString(feld.length, feld);
-                Frame.setPlayground();
+                FrameAPI.initString(feld.length, feld);
+                FrameAPI.setPlayground();
             }
 
-        } while (!Frame.allDead);
+        } while (!FrameAPI.allDead);
 
         // TODO code application logic here
     }
@@ -62,29 +56,12 @@ public class FinnischesHasenprojekt {
         feld[8][7] = "H";
         feld[3][12] = "F";
         feld[1][4] = "H";
-        feld[3][2] = "J";
+        feld[3][5] = "J";
         feld[7][9] = "u";
         feld[2][2] = "R";
         feld[5][1] = "H";
-        feld[3][1] = "H";
-        
-    }
+        feld[3][6] = "H";
 
-    private static void moveHasen() {
-
-        for (int i = 0; i < feld.length; i++) {
-            for (int j = 0; j < feld.length; j++) {
-                Speicher[i][j] = feld[i][j];
-            }
-        }
-        for (int i = 0; i < feld.length; i++) {
-            for (int j = 0; j < feld.length; j++) {
-                if (Speicher[i][j].equals("H")) {
-                    feld[i][j] = ".";
-                    feld[i][j + 1] = "H";
-                }
-            }
-        }
     }
 
     private static void moveFüchse() {
@@ -108,45 +85,39 @@ public class FinnischesHasenprojekt {
                     feld[i][j - 1] = "J";
                 }
             }
-         
+
         }
-          for (int i = 0; i < feld.length; i++) {
+        for (int i = 0; i < feld.length; i++) {
             for (int j = 0; j < feld.length; j++) {
                 if (Speicher[i][j].equals("u")) {
                     feld[i][j] = ".";
-                    feld[i][j -1 ] = "u";
+                    feld[i][j - 1] = "u";
                 }
             }
         }
     }
-<<<<<<< HEAD
 
     private static void moveHasen() {
-
-        for (x = 0; x < 14; x++) {
-
-            for (y = 0; y < 14; y++) {
-
+        
+        for ( x=0; x< 14; x++) {
+            
+            for ( y=0; y<14; y++) {
+                
                 feld[x][y] = Speicher[x][y];
+                
 
             }
         }
-        if (feld[x][y] == "H") {
+             if(feld[x][y] == "H") {
                 // double zufallszahl = Math.random();
-            //  if(zufallszahl)>
+               //  if(zufallszahl)>
+                 
+             } else {
+             }
+                Speicher [x][y] = ".";
+                Speicher [x][y] = "H";
+                
+             
 
-        } else {
-        }
-        Speicher[x][y] = ".";
-        Speicher[x][y] = "H";
-
-=======
-    
-    private static void Steuerung()
-    {
-        
-        
-        
->>>>>>> parent of dbe3299... Zufallszahlen
     }
 }
